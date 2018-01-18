3bc6b1b13e9dd2a7c1fc7a49c1d76bfdf361cca7
Adaming44 <Adaming44@192.168.1.145>
2018-01-18 13:02:33 +0100

    ccc

--- 12_Projet_E_Commerce_Spring/src/main/java/fr/adaming/managedBeans/ProduitManagedBean.java
+++ 12_Projet_E_Commerce_Spring/src/main/java/fr/adaming/managedBeans/ProduitManagedBean.java
@@ -19,6 +19,7 @@ import org.primefaces.model.UploadedFile;
 
 import fr.adaming.model.Categorie;
 import fr.adaming.model.Produit;
+import fr.adaming.service.ICategorieService;
 import fr.adaming.service.IProduitService;
 
 @ManagedBean(name = "pMB")
@@ -29,6 +30,9 @@ public class ProduitManagedBean implements Serializable {
 
 	@ManagedProperty(value = "#{pdtService}")
 	private IProduitService produitService;
+
+	@ManagedProperty(value = "#{catService}")
+	private ICategorieService categorieService;
 
 	private Produit produit;
 	private List<Produit> listeProduit;
@@ -88,92 +92,106 @@ public class ProduitManagedBean implements Serializable {
 		this.image = image;
 	}
 
+	public void setCategorieService(ICategorieService categorieService) {
+		this.categorieService = categorieService;
+	}
+
 	// methodes
 	public String ajouterProduit() {
 
-		this.produit = produitService.addProduit(this.produit, this.categorie);
+		this.categorie = categorieService.getCategorieByIdOrName(this.categorie);
 
-		if (this.produit != null) {
-			// récupération de la nouvelle liste de la bd
-			List<Produit> listOut = produitService.getProduitByCat(this.categorie);
-			this.listeProduit = new ArrayList<Produit>();
+		if (this.categorie != null) {
+			this.produit = produitService.addProduit(this.produit, this.categorie);
 
-			for (Produit element : listOut) {
-				if (element.getPhoto() == null) {
-					element.setImage(null);
-				} else {
-					element.setImage("data:image/jpeg;base64," + Base64.encodeBase64String(element.getPhoto()));
+			if (this.produit != null) {
+				// récupération de la nouvelle liste de la bd
+				List<Produit> listOut = produitService.getProduitByCat(this.categorie);
+				this.listeProduit = new ArrayList<Produit>();
+
+				for (Produit element : listOut) {
+					if (element.getPhoto() == null) {
+						element.setImage(null);
+					} else {
+						element.setImage("data:image/jpeg;base64," + Base64.encodeBase64String(element.getPhoto()));
+					}
+					this.listeProduit.add(element);
 				}
-				this.listeProduit.add(element);
+
+				// mettre à jour la liste dans la session
+				maSession.setAttribute("produitList", this.listeProduit);
+
+				return "afficheProduit";
+			} else {
+				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("L'ajout a échoué"));
+				return "ajoutProduit";
 			}
-
-			// mettre à jour la liste dans la session
-			maSession.setAttribute("produitList", this.listeProduit);
-
-			return "afficheProduit";
 		} else {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("L'ajout a échoué"));
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Cette catégorie n'existe pas !"));
 			return "ajoutProduit";
 		}
 	}
 
-//	public String afficherProduit() {
-//		Produit pFind = produitService.getProduitbyIdorName(this.produit);
-//
-//		if (pFind != null) {
-//			if (pFind.getPhoto() == null) {
-//				pFind.setImage(null);
-//			} else {
-//				//pFind.setImage("data:image/jpeg;base64," + Base64.encodeBase64String(pFind.getPhoto()));
-//			}
-//
-//			this.produit = pFind;
-//			maSession.setAttribute("produit", this.produit);
-//
-//			return "afficheProduit";
-//		} else {
-//			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Liste vide"));
-//			return "afficheProduit";
-//		}
-//	}
+	public String afficherProduit() {
+		Produit pFind = produitService.getProduitbyIdorName(this.produit);
 
-//	public String supprimerProduit() {
-//		int verif = produitService.deleteProduit(this.produit.getIdProduit());
-//		if (verif == 1) {
-//			return "rechercheCat";
-//
-//		} else {
-//			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("echec suppression"));
-//			return "supprProduit";
-//		}
-//
-//	}
+		if (pFind != null) {
+			if (pFind.getPhoto() == null) {
+				pFind.setImage(null);
+			} else {
+				pFind.setImage("data:image/jpeg;base64," + Base64.encodeBase64String(pFind.getPhoto()));
+			}
 
-//	public String modifierProduit() {
-//		this.produit = produitService.updateProduit(this.produit, this.categorie);
-//		if (this.produit != null) {
-//			// récupération de la nouvelle liste de la bd
-//			List<Produit> listOut = produitService.getProduitByCat(this.categorie);
-//			this.listeProduit = new ArrayList<Produit>();
-//
-//			for (Produit element : listOut) {
-//				if (element.getPhoto() == null) {
-//					element.setImage(null);
-//				} else {
-//					//element.setImage("data:image/jpeg;base64," + Base64.encodeBase64String(element.getPhoto()));
-//				}
-//				this.listeProduit.add(element);
-//			}
-//
-//			// mettre à jour la liste dans la session
-//			maSession.setAttribute("produitList", this.listeProduit);
-//
-//			return "rechercheCat";
-//		} else {
-//			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("echec modification"));
-//			return "modifProduit";
-//		}
-//	}
+			this.produit = pFind;
+			maSession.setAttribute("produit", this.produit);
+
+			return "afficheProduit";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Ce produit n'existe pas !"));
+			return "afficheProduit";
+		}
+	}
+
+	public String supprimerProduit() {
+		int verif = produitService.deleteProduit(this.produit.getIdProduit());
+		if (verif == 1) {
+			return "rechercheCat";
+
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("echec suppression"));
+			return "supprProduit";
+		}
+
+	}
+
+	// public String modifierProduit() {
+	// this.produit = produitService.updateProduit(this.produit,
+	// this.categorie);
+	// if (this.produit != null) {
+	// // récupération de la nouvelle liste de la bd
+	// List<Produit> listOut = produitService.getProduitByCat(this.categorie);
+	// this.listeProduit = new ArrayList<Produit>();
+	//
+	// for (Produit element : listOut) {
+	// if (element.getPhoto() == null) {
+	// element.setImage(null);
+	// } else {
+	// //element.setImage("data:image/jpeg;base64," +
+	// Base64.encodeBase64String(element.getPhoto()));
+	// }
+	// this.listeProduit.add(element);
+	// }
+	//
+	// // mettre à jour la liste dans la session
+	// maSession.setAttribute("produitList", this.listeProduit);
+	//
+	// return "rechercheCat";
+	// } else {
+	// FacesContext.getCurrentInstance().addMessage(null, new
+	// FacesMessage("echec modification"));
+	// return "modifProduit";
+	// }
+	// }
 
 	// méthode pour transformer une image en table de byte
 	public void upload(FileUploadEvent event) {
